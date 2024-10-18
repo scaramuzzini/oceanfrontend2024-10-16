e64f214a64714e80e4df64b086391aed2d2ce231
Mario Torres <scaramuzzini@gmail.com>
2024-10-18 10:12:33 -0400

    Tabuleiro com cliques

--- src/JogoDaVelha.js
+++ src/JogoDaVelha.js
@@ -1,21 +1,41 @@
+import { useState } from "react";
+
+function Quadrado({quadrado, cliqueQuadrado}) {
+    return (
+        <button class="quadrado" 
+            onClick={cliqueQuadrado}>
+            {quadrado}
+        </button>
+    );
+}
+
 function Jogo() {
+    const [quad,setQuad] = useState('');
+    const [quadrados, setQuadrados] = useState(Array(9).fill(null));
+    function handleClick(indice) {
+        const novosQuadrados = quadrados.slice();
+        novosQuadrados[indice] = 'X';
+        setQuadrados(novosQuadrados);
+    }
+
+
     return (
         <>
             <h1>Jogo da velha</h1>
             <div class="linha">
-                <button class="quadrado">X</button>
-                <button>X</button>
-                <button>X</button>
+                <Quadrado quadrado={quadrados[0]} cliqueQuadrado={() => handleClick(0)} />
+                <Quadrado quadrado={quadrados[1]} cliqueQuadrado={() => handleClick(1)}/>
+                <Quadrado quadrado={quadrados[2]} cliqueQuadrado={() => handleClick(2)}/>
             </div>
             <div class="linha">
-                <button>X</button>
-                <button>X</button>
-                <button>X</button>
+                <Quadrado quadrado={quadrados[3]} cliqueQuadrado={() => handleClick(3)}/>
+                <Quadrado quadrado={quadrados[4]} cliqueQuadrado={() => handleClick(4)}/>
+                <Quadrado quadrado={quadrados[5]} cliqueQuadrado={() => handleClick(5)}/>
             </div>
             <div class="linha">
-                <button>X</button>
-                <button>X</button>
-                <button>X</button>
+                <Quadrado quadrado={quadrados[6]} cliqueQuadrado={() => handleClick(6)}/>
+                <Quadrado quadrado={quadrados[7]} cliqueQuadrado={() => handleClick(7)}/>
+                <Quadrado quadrado={quadrados[8]} cliqueQuadrado={() => handleClick(8)}/>
             </div>
         </>
     );
