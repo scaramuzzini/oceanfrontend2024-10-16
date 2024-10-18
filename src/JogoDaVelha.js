e944ae7a30621b7dfc0b9ab278906ac79120df54
Mario Torres <scaramuzzini@gmail.com>
2024-10-18 09:30:07 -0400

    Tabuleiro inicial

--- src/JogoDaVelha.js
+++ src/JogoDaVelha.js
@@ -1,6 +1,23 @@
 function Jogo() {
     return (
-        <h1>Jogo da velha</h1>
+        <>
+            <h1>Jogo da velha</h1>
+            <div class="linha">
+                <button class="quadrado">X</button>
+                <button>X</button>
+                <button>X</button>
+            </div>
+            <div class="linha">
+                <button>X</button>
+                <button>X</button>
+                <button>X</button>
+            </div>
+            <div class="linha">
+                <button>X</button>
+                <button>X</button>
+                <button>X</button>
+            </div>
+        </>
     );
 }
 
