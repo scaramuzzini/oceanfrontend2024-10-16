3cc60f08c5290c2ccae0c7ceaad2a7730d8d1145
Mario Torres <scaramuzzini@gmail.com>
2024-10-18 11:10:06 -0400

    Jogo básico finalizado

--- src/JogoDaVelha.js
+++ src/JogoDaVelha.js
@@ -11,11 +11,14 @@ function Quadrado({quadrado, cliqueQuadrado}) {
 
 function Tabuleiro({vezDoX, quadrados, handleClick}) {
 
+    const vencedor = verificarVencedor(quadrados);
+
+
     let mensagem = 'oi';
-    if (vezDoX) {
-        mensagem = 'Vez do X';
+    if (vencedor) {
+        mensagem = 'O vencedor é: ' + vencedor;
     } else {
-        mensagem = 'Vez da O';
+        mensagem = 'Vez do ' + (vezDoX ? 'X' : 'O');
     }
 
     return (
@@ -41,11 +44,43 @@ function Tabuleiro({vezDoX, quadrados, handleClick}) {
 
 }
 
+function verificarVencedor(quadrados) {
+
+  
+    const combinacoesPremiadas = [
+        //linhas
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        //colunas
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        //diagonais (dp e ds)
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i=0; i < combinacoesPremiadas.length; i++) {
+        const [pa, pb, pc] = combinacoesPremiadas[i];
+        
+        if (quadrados[pa] && 
+            quadrados[pa] == quadrados[pb]
+            && quadrados[pb] == quadrados[pc]
+        ) {
+            return quadrados[pa];
+        }
+    }
+
+    return null;
+
+}
+
 function Jogo() {
     const [quadrados, setQuadrados] = useState(Array(9).fill(null));
     const [vezDoX,setVezDoX] = useState(true);
     function handleClick(indice) { // indice=0
-        if (quadrados[indice]) { //quadrados[0]
+        if (verificarVencedor(quadrados) || quadrados[indice]) { //quadrados[0]
             return; // saida da funcao
         }
         const novosQuadrados = quadrados.slice();
@@ -54,11 +89,15 @@ function Jogo() {
         setVezDoX(!vezDoX);
     }
 
-    //Adicionar botao RESET DO JOGO
+    function resetGame() {
+        setQuadrados(Array(9).fill(null));
+        setVezDoX(true);
+    }
+
     return (
         <>
             <h1>Jogo da velha</h1>
-            <button>Resetar jogo</button>
+            <button onClick={resetGame}>Reiniciar jogo</button>
             <Tabuleiro vezDoX={vezDoX} 
                 quadrados={quadrados} 
                 handleClick={handleClick} />
